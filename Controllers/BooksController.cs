52a513848032e6f7246e2b603d3b0bf0cd9f0b58
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-05-26 12:05:15 +0700

    Create the BooksController. And Implement Get /api/books

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BooksController : ControllerBase
+    {
+        List<Book> Books = new List<Book>
+        {
+            new Book { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Year = 1925, Genre = "Novel" },
+            new Book { Id = 2, Title = "To Kill a Mockingbird", Author = "Harper Lee", Year = 1960, Genre = "Novel" },
+            new Book { Id = 3, Title = "1984", Author = "George Orwell", Year = 1949, Genre = "Dystopian" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetAllBooks()
+        {
+            return Books;
+        }
+
+
+
+    }
+}
